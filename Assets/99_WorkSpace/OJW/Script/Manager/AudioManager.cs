7e7f515a0a9418da75f886fb7c45e168851a0a7d
Jaewon <dhwodnjs0827@naver.com>
2025-03-12 16:36:35 +0900

    [Feat] 컴포넌트 제너릭 형식의 싱글톤 추가
    # AudioManager.cs
    - Singleton 상속
    
    # BaseManager.cs
    - BaseManager에서 Singleton으로 이름 변경
    
    # GameManager.cs
    - Singleton 상속
    
    # Singleton.cs
    - 컴포넌트 제너릭 형식의 싱글톤 기능 추가
    
    # UIManager.cs
    - Singleton 상속

--- Assets/99_WorkSpace/OJW/Script/Manager/AudioManager.cs
+++ Assets/99_WorkSpace/OJW/Script/Manager/AudioManager.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AudioManager : MonoBehaviour
+public class AudioManager : Singleton<AudioManager>
 {
 
 }
