4cd529d716baa480619a72bc86cc3767bc6b450b
Jaewon <dhwodnjs0827@naver.com>
2025-03-12 20:35:26 +0900

    [Feat] UI 전환 기능 추가
    # UIManager.cs
    - UI 클래스들 필드에 추가
    - BaseUI를 상속 받은 클래스를 관리하기 위한 List 추가
    - UIType에 따른 UI 전환 기능 추가

--- Assets/99_WorkSpace/OJW/Script/Manager/UIManager.cs
+++ Assets/99_WorkSpace/OJW/Script/Manager/UIManager.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
+using DataDeclaration;
 using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
     private bool isCursorOn;
+
+    public List<BaseUI> UIList { get; private set; }
+    public LobbyUI LobbyUI { get; private set; }
+    public SettingUI SettingUI { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        
+        UIList = new List<BaseUI>();
+        
+        LobbyUI = InitUI<LobbyUI>();
+        SettingUI = InitUI<SettingUI>();
+    }
 
     /// <summary>
     /// 마우스 커서 On/Off 기능
@@ -11,5 +27,27 @@ public class UIManager : Singleton<UIManager>
     public static void ToggleCursor(bool isActive)
     {
         Cursor.lockState = isActive ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+
+    public void ChangeUIState(UIType type)
+    {
+        foreach (var ui in UIList)
+        {
+            ui.ActiveUI(type);
+        }
+    }
+
+    private T InitUI<T>() where T : BaseUI
+    {
+        var ui = GetComponentInChildren<T>(true);
+        if (ui == null)
+        {
+            var go = new GameObject(nameof(T));
+            go.transform.SetParent(transform);
+            ui = go.AddComponent<T>();
+            
+        }
+        ui.Init(this);
+        return ui;
     }
 }
