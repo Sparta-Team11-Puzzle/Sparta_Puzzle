b1fe9c2e9e61a185e616b192035db801d9a39178
kaffu <kaffu@naver.com>
2025-03-12 15:53:03 +0900

    [Feat] BaseRoom / DungeonManager 추가
    # DungeonManager.cs
     - BaseRoom 참조 저장
     - BaseRoom 초기화
     - BaseRoom 로직실행
    
    # BaseRoom.cs
     - Room 추상 클래스

---
+++ Assets/03_Scripts/Puzzle/BaseRoom.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseRoom : MonoBehaviour
+{
+    protected bool isClear;             // 방 클리어 여부
+    protected bool roomInPlayer;        // 플레이어 존재 여부
+
+    protected GameObject roomObjects;   // 퍼즐 오브젝트
+    protected Transform player;         // 플레이어 Transform
+
+    protected DungeonManager dungeonManager;
+
+    public abstract void InitRoom(DungeonManager manager);
+    public abstract void UpdateRoom();
+}
---
+++ Assets/03_Scripts/Puzzle/DungeonManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Stage
+{
+    // 수정 필요하면 수정하기
+    Stage1,
+    Stage2,
+    Stage3,
+}
+public class DungeonManager : MonoBehaviour
+{
+    private BaseRoom[] baseRooms;
+    private BaseRoom currentRoom;
+    
+    void Start()
+    {
+        for (int i = 0; i < baseRooms.Length; i++)
+        {
+            baseRooms[i].InitRoom(this);
+        }
+    }
+
+    void Update()
+    {
+        if (currentRoom == null)
+            return;
+
+        currentRoom.UpdateRoom();
+    }
+
+    // #TODO
+    // 룸 변경 함수 추가하기
+    // 
+}
