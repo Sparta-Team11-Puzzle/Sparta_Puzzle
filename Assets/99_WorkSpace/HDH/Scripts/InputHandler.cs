a795a605848a71d55c426655383f407ec6285f37
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-03-12 16:33:40 +0900

    [Feat] Camera 조작
    # PlayerInput.inputactions
    - 키 입력 설정
    
    # InputHandler.cs
    - InputAction의 입력값을 받는 클래스
    
    # PlayerController.cs
    - 마우스 이동에 따른 카메라 조작 추가

---
+++ Assets/99_WorkSpace/HDH/Scripts/InputHandler.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    //InputAction을 통한 키 입력을 처리
+    private PlayerInput input;
+    private InputActionMap playerActionMap;
+    public Vector2 movementInput { get; private set; }
+    public Vector2 mouseDelta { get; private set; }
+    public bool isRun { get; private set; }
+    public bool isUse { get; private set; }
+
+    public event Action OnJump;
+
+    void Start()
+    {
+        input = GetComponent<PlayerInput>();
+        playerActionMap = input.actions.FindActionMap("Player");
+
+        InitInputActions();
+    }
+
+    void InitInputActions()
+    {
+        InputAction moveAction = playerActionMap.FindAction("Move");
+        moveAction.performed += context => movementInput = context.ReadValue<Vector2>();
+        moveAction.canceled += context => movementInput = Vector2.zero;
+
+        InputAction jumpAction = playerActionMap.FindAction("Jump");
+        jumpAction.started += context => OnJump?.Invoke();
+
+        InputAction lookAction = playerActionMap.FindAction("Mouse");
+        lookAction.performed += context => mouseDelta = context.ReadValue<Vector2>();
+
+        InputAction runAction = playerActionMap.FindAction("Run");
+        runAction.performed += context => isRun = true;
+        runAction.canceled += context => isRun = false;
+
+        InputAction useAction = playerActionMap.FindAction("Use");
+        useAction.performed += context => isUse = true;
+        useAction.canceled += context => isUse = false;
+    }
+}
--- Assets/99_WorkSpace/HDH/Scripts/PlayerController.cs
+++ Assets/99_WorkSpace/HDH/Scripts/PlayerController.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
+    private InputHandler inputHandler;
+
+    [Header("Camera Info")]
+    [SerializeField] float cameraSensitivity;
+    private float camRotX;
+    private float camRotY;
+    private Camera camera;
+    [SerializeField] private float maxRotX;
+    [SerializeField] private float minRotX;
+    [SerializeField] Transform fpsCameraTransform;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        inputHandler = GetComponent<InputHandler>();
+        camera = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
         
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        float mouseX = inputHandler.mouseDelta.x * Time.deltaTime * cameraSensitivity;
+        float mouseY = inputHandler.mouseDelta.y * Time.deltaTime * cameraSensitivity;
+
+        camRotY += mouseX;
+        camRotX -= mouseY;
+
+        camRotX = Mathf.Clamp(camRotX, minRotX, maxRotX);
+
+        camera.transform.position = fpsCameraTransform.position;
+        camera.transform.rotation = Quaternion.Euler(camRotX, camRotY, 0);
+        transform.rotation = Quaternion.Euler(0, camRotY, 0);
     }
 }
