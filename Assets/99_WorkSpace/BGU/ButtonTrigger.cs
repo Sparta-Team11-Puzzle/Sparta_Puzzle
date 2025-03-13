51b6c07864eacc66bb9c3ed8e4d056f8d99bd1d1
100ZADA <5528asd@gmail.com>
2025-03-13 14:21:11 +0900

    [Feat] Scene 오브젝트 추가
    # Stage3TestScene.unity
     - 버튼 Object 및 장애물 Object 추가
    
    # ButtonTrigger.cs
     - 버튼 Object Script 추가
    
    # InvisibleTriggerObject.cs
     - 사라지는 Script 추가

---
+++ Assets/99_WorkSpace/BGU/ButtonTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    public List<InvisibleTriggerObject> targaetObject;          // 활성화 시킬 오브젝트 리스트
+
+    /// <summary>
+    /// 오브젝트 보이게 하기
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (InvisibleTriggerObject obj in targaetObject)
+            {
+                obj.OnVisible();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 오브젝트 안보이게 하기
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (InvisibleTriggerObject obj in targaetObject)
+            {
+                obj.Invisible();
+            }
+        }
+    }
+}
---
+++ Assets/99_WorkSpace/BGU/InvisibleTriggerObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleTriggerObject : MonoBehaviour
+{
+    private Renderer objectRenderer;            // 렌더러 컴포넌트
+    private Color originalColor;                // 원래 색상
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        originalColor = objectRenderer.material.color;          // 원래 색상 저장
+
+        Invisible();            // 시작시 투명
+    }
+
+    /// <summary>
+    /// 오브젝트를 안보이게 만들기
+    /// </summary>
+    public void Invisible()
+    {
+        Color tempColor = originalColor;            // 원래 색상 복사
+        tempColor.a = 0f;                           // 알파 값을 0으로 설정
+        objectRenderer.material.color = tempColor;  // 변경된 색상 적용
+    }
+
+    /// <summary>
+    /// 오브젝트를 보이게 만들기
+    /// </summary>
+    public void OnVisible()
+    {
+        objectRenderer.material.color = originalColor;      // 원래 색상으로 복원
+    }
+}
