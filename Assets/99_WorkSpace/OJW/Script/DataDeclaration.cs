97c5e92af20a4d73dd44c1f679c0b20c4fef0548
Jaewon <dhwodnjs0827@naver.com>
2025-03-13 10:51:31 +0900

    [Feat] AudioManager 기능 추가
    # DataDeclaration.cs
    - 상수 추가
    
    # AudioManager.cs
    - 배경음악 추가 및 재생
    - 사운드 조절 기능 추가

--- Assets/99_WorkSpace/OJW/Script/DataDeclaration.cs
+++ Assets/99_WorkSpace/OJW/Script/DataDeclaration.cs
@@ -19,4 +19,11 @@ namespace DataDeclaration
         Sound,
         Key,
     }
+
+    public static class Constant
+    {
+        public const string MASTER_VOL = "masterVol";
+        public const string BGM_VOL = "bgmVol";
+        public const string SFX_VOL = "sfxVol";
+    }
 }
--- Assets/99_WorkSpace/OJW/Script/Manager/AudioManager.cs
+++ Assets/99_WorkSpace/OJW/Script/Manager/AudioManager.cs
@@ -1,4 +1,65 @@
+using System.Collections.Generic;
+using DataDeclaration;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : Singleton<AudioManager>
 {
+    private List<AudioSource> sfxList;
 
+    private float masterVol;
+    private float bgmVol;
+    private float sfxVol;
+
+    private AudioSource bgm;
+    public AudioClip bgmClip;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        
+        sfxList =  new List<AudioSource>();
+        
+        masterVol = 0.8f;
+        bgmVol = 0.8f;
+        sfxVol = 0.8f;
+        
+        bgm = GetComponent<AudioSource>();
+        bgm.clip = bgmClip;
+        bgm.volume = bgmVol * masterVol;
+        
+        PlayerPrefs.SetFloat(Constant.MASTER_VOL, masterVol);
+        PlayerPrefs.SetFloat(Constant.BGM_VOL, bgmVol);
+        PlayerPrefs.SetFloat(Constant.SFX_VOL, sfxVol);
+    }
+
+    private void Start()
+    {
+        bgm.Play();
+    }
+
+    public void ChangeMasterVol(float figure)
+    {
+        masterVol = figure;
+        bgm.volume = bgmVol * masterVol;
+        foreach (var sfx in sfxList)
+        {
+            sfx.volume = sfxVol * masterVol;
+        }
+    }
+
+    public void ChangeBGMVol(float figure)
+    {
+        bgmVol = figure;
+        bgm.volume = bgmVol * masterVol;
+    }
+    
+    public void ChangeSFXVolume(float figure)
+    {
+        sfxVol = figure;
+        foreach (var sfx in sfxList)
+        {
+            sfx.volume = sfxVol * masterVol;
+        }
+    }
 }
