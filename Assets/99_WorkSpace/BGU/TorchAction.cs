3160bd9a8c29b2cc7ebc61af928797bbd96c9fe9
100ZADA <5528asd@gmail.com>
2025-03-13 16:12:33 +0900

    [Feat] 오브젝트 상호작용 추가
    # Stage3TestScene.unity
     - Torch 오브젝트 추가
    
    # TorchAction.cs
     - 상호작용 시 동작하는 코드 추가

---
+++ Assets/99_WorkSpace/BGU/TorchAction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchAction : MonoBehaviour
+{
+    public GameObject fireEffect;       // 불 효과 오브젝트
+    public bool isLight = false;          // 불이 켜진 상태 확인
+    public static int lightTorchCoiunt = 0;   // 켜진 횃불 개수
+
+    void Start()
+    {
+        fireEffect.SetActive(false);        // 초기 상태 비활성화
+    }
+
+    public void ToggleFire()
+    {
+        // 불이 꺼진 상태
+        if (!isLight)
+        {
+            fireEffect.SetActive(true);
+            isLight = true;
+            lightTorchCoiunt++;
+        }
+        else
+        {
+            fireEffect.SetActive(false);
+            isLight = false;
+            lightTorchCoiunt--;
+        }
+
+        // 켜진 횃불이 2개라면
+        if (lightTorchCoiunt == 2)
+        {
+            // "Door" 이름의 오브젝트 비활성화
+            GameObject.Find("Door").SetActive(false);
+        }
+    }
+}
