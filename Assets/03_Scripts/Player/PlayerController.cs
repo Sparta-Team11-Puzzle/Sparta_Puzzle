a9ea5f7b746c5dee0351b7e3a04c9cdb984fb7af
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-03-14 19:39:30 +0900

    [Feat] 지면 확인 Ray 추가
    # PlayerController.cs
    - 지면 확인하는 Ray 갯수를 추가

--- Assets/03_Scripts/Player/PlayerController.cs
+++ Assets/03_Scripts/Player/PlayerController.cs
@@ -47,6 +47,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject pushingObject;
     [SerializeField] private bool isPushing;
 
+    private bool startJump;
+
     /// <summary>
     /// 플레이어의 정면 방향을 반환
     /// </summary>
@@ -93,6 +95,8 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         GroundCheck();
+
+        startJump = inputHandler.InputJump;
     }
 
     private void FixedUpdate()
@@ -119,18 +123,25 @@ public class PlayerController : MonoBehaviour
 
     void GroundCheck()
     {
-        Ray ray = new Ray(transform.position + Vector3.up * 0.1f, Vector3.down);
+        Ray[] rays = new Ray[4]
+            {
+            new Ray(transform.position  + (transform.forward * 0.2f) + Vector3.up * 0.1f, Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + Vector3.up * 0.1f, Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + Vector3.up * 0.1f, Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + Vector3.up * 0.1f, Vector3.down)
+            };
 
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, distanceToGround, groundLayer))
+        foreach (Ray ray in rays)
         {
-            isGround = true;
+            if (Physics.Raycast(ray, out hit, distanceToGround, groundLayer))
+            {
+                isGround = true;
+            }
         }
-        else
-        {
-            isGround = false;
-        }
+
+        isGround = false;
     }
 
     void FPSMove(Vector2 movementInput)
@@ -263,10 +274,8 @@ public class PlayerController : MonoBehaviour
 
     void OnJump()
     {
-        if (inputHandler.InputJump && isGround && canMove)
+        if (startJump && isGround && canMove)
         {
-            Debug.Log(inputHandler.InputJump);
-
             if(!readyToJump)
             {
                 Jump();
@@ -280,7 +289,7 @@ public class PlayerController : MonoBehaviour
         }
 
     }
-
+    
     void Jump()
     {
         rigidbody.AddForce(transform.up * playerData.JumpForce, ForceMode.Impulse);
