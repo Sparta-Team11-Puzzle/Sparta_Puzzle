801178ff3cc08b4fb2a8f7fec1b8761af186ad03
100ZADA <5528asd@gmail.com>
2025-03-14 15:06:43 +0900

    [Style] 오류 삭제
    # Stage3.cs
     - 사용하지 않는 코드 삭제

--- Assets/99_WorkSpace/BGU/Scripts/Stage3.cs
+++ Assets/99_WorkSpace/BGU/Scripts/Stage3.cs
@@ -1,15 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Stage3 : BaseRoom
 {
-    public override void UpdateRoom()
-    {
-        // 작성안함
-        throw new System.NotImplementedException();
-    }
-
     // 방 초기화 할때 플레이어한테 PushAction 붙이기
     public override void InitRoom(DungeonManager manager)
     {
