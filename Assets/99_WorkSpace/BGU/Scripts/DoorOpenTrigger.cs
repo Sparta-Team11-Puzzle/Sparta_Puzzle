88e264ac265b9cd3a57730dfd1ca00d43a854e7f
100ZADA <5528asd@gmail.com>
2025-03-14 10:11:55 +0900

    [Feat] Door Open 기능 추가
    # DoorOpenTrigger.cs
     - Object 충돌 시 Door Open 코드 추가

--- Assets/99_WorkSpace/BGU/Scripts/DoorOpenTrigger.cs
+++ Assets/99_WorkSpace/BGU/Scripts/DoorOpenTrigger.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorOpenTrigger : MonoBehaviour
+public class DoorOpenTrigger : MonoBehaviour, IInteractable
 {
-    public string targetTag;
     [SerializeField] private GameObject openObject;
+    public string targetTag;
     private IEventTrigger targetEvent;
+    private bool isTriggered = false;           // 트리거 상태 확인
 
     void Start()
     {
@@ -16,8 +17,22 @@ public class DoorOpenTrigger : MonoBehaviour
         targetEvent = openObject.GetComponent<IEventTrigger>();
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (isTriggered) return;    // 이미 트리거 시 무시
+
+        if (other.CompareTag(targetTag))
+        {
+            isTriggered = true;     // 트리거 표시
+            targetEvent?.EventTrigger();
+        }
+    }
+
     public void Interact()
     {
+        if (isTriggered) return;
+
+        isTriggered = true;     // 트리거 표시
         targetEvent?.EventTrigger();
     }
 }
