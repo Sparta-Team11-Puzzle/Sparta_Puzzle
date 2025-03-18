f5bd29f9eebb08b19a130ae07a73b4bb8b0422e8
kaffu <kaffu@naver.com>
2025-03-18 09:14:24 +0900

    [Feat] 플레이어 추력 사운드 추가
    # DeadPlane.cs
     - 추력시 사운드 출력

--- Assets/03_Scripts/Puzzle/DeadPlane.cs
+++ Assets/03_Scripts/Puzzle/DeadPlane.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class DeadPlane : MonoBehaviour
 {
     [SerializeField] private Transform respawnPoint;
+    private PlayerSFXController controller;
 
+    private void Start()
+    {
+        controller = FindObjectOfType<PlayerSFXController>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             other.transform.position = respawnPoint.position;
+            controller.PlayDieSound();
         }
     }
 }
