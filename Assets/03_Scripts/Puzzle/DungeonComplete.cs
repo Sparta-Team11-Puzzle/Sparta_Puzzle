e6adb5ff65886dd4f3bdbefe81068b9cbd034ffc
kaffu <kaffu@naver.com>
2025-03-18 10:05:51 +0900

    [Fix] 스테이지 진행도 저장위치 수정

--- Assets/03_Scripts/Puzzle/DungeonComplete.cs
+++ Assets/03_Scripts/Puzzle/DungeonComplete.cs
@@ -3,21 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.SceneManagement;
 
 public class DungeonComplete : MonoBehaviour, IInteractable
 {
-    private string sceneNama = "00_Lobby";
+
+    [SerializeField] private GameObject targetObject;
+    private IEventTrigger saveEvent;
+
+    private void Start()
+    {
+        saveEvent = targetObject.GetComponent<IEventTrigger>();
+    }
 
     public void Interact()
     {
+        saveEvent.EventTrigger();
         UIManager.Instance.Fade(0, 1, 3, EndGame);
     }
 
     private void EndGame()
     {
-        //Cursor.lockState = CursorLockMode.None;
-        //SceneManager.LoadScene(sceneNama);
         GameManager.ChangeScene(SceneType.Ending);
     }
 }
